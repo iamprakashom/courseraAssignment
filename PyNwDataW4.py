0bf5ad2acdc96797cfb258fd5300cd4d4c3ab027
Om Prakash <omprakash070@gmail.com>
2016-08-05 19:45:18 +0530

    more tag functionality added

--- PyNwDataW4.py
+++ PyNwDataW4.py
@@ -1,6 +1,7 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 
+# Change the given url if needed
 urldata = urlopen('http://python-data.dr-chuck.net/comments_42.html')
 soupdata = BeautifulSoup(urldata.read())
 
@@ -9,5 +10,14 @@ sum = 0
 # Adding all the numerical value given on above url
 for tgs in tag:
     sum += int(tgs.contents[0])
+
+
+tagsA = soupdata.find_all('a')
+for tg in tagsA:
+    print ("TAG", tg)
+    print("URL", tg.get('href', None))
+    print('Contents:', tg.contents[0])
+    print('Attrs', tg.attrs)
+    print(tg)
 
 print(sum)
